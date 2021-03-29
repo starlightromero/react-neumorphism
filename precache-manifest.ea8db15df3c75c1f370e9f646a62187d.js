7311015725d35deb41a1e83528312cf9a9382ae2
Starlight Romero <starlightromero@protonmail.com>
2021-03-28 23:51:23 -0700

    Updates

---
+++ precache-manifest.ea8db15df3c75c1f370e9f646a62187d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc66dd8600ce3bb12800b674e153867",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7bbc328bd03887403ff4",
+    "url": "./static/css/main.6525b788.chunk.css"
+  },
+  {
+    "revision": "dd813ccac3fb3e01a6f1",
+    "url": "./static/js/2.4a4269b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4a4269b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbc328bd03887403ff4",
+    "url": "./static/js/main.f88954e5.chunk.js"
+  },
+  {
+    "revision": "20ed8f88a8507040fa22",
+    "url": "./static/js/runtime-main.f6e9f142.js"
+  }
+]);
